10a0a17f61ba1fe8b66034a69e47ff3c530b5174
William Zheng <williamzheng2001@gmail.com>
2025-12-02 18:02:47 -0500

    2025 day 2 part 2

--- 2025/02/2025-12-02/Main.hs
+++ 2025/02/2025-12-02/Main.hs
@@ -3,7 +3,12 @@ module Main where
 import Data.Int
 import Data.List.Split
 
-type Range = (Int64, Int64)
+type Id = Int64
+
+type Range = (Id, Id)
+
+countDigits :: Id -> Int64
+countDigits = ceiling . logBase 10 . fromIntegral
 
 sumInvalidIds :: [Range] -> Int64
 sumInvalidIds =
@@ -11,18 +16,26 @@ sumInvalidIds =
       f range acc = acc + sumIdsInRange range
    in foldr f 0
 
-isInvalidId :: Int64 -> Bool
+isInvalidId :: Id -> Bool
 isInvalidId id =
-  let pow = (`div` 2) . ceiling . logBase 10 . fromIntegral $ id
-      divisor = 10 ^ pow
-      left = id `div` divisor
-      right = id `mod` divisor
-   in left == right
+  let maxLen = (`div` 2) . countDigits $ id
+      f seqLen acc = acc || isRepeatingSeq seqLen id
+   in foldr f False [1 .. maxLen]
+
+isRepeatingSeq :: Int64 -> Id -> Bool
+isRepeatingSeq seqLen id =
+  let digits = countDigits id
+      divisor = 10 ^ seqLen
+      seq = id `mod` divisor
+      isRepeating id
+        | id > 0 = (id `mod` divisor == seq) && isRepeating (id `div` divisor)
+        | otherwise = True
+   in digits `mod` seqLen == 0 && isRepeating id && id /= seq
 
 parseContents :: String -> [Range]
 parseContents =
   let parseRange s = case splitOn "-" s of
-        (x : y : []) -> (read x :: Int64, read y :: Int64)
+        (x : y : []) -> (read x :: Id, read y :: Id)
         _ -> error "Invalid range"
    in map parseRange . splitOn ","
 
