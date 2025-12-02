f46c9519b1c9b2c36075e620a837d1ab939e8f2d
William Zheng <williamzheng2001@gmail.com>
2025-12-01 20:28:52 -0500

    2025 day 1 part 2

--- 2025/01/2025-12-01/Main.hs
+++ 2025/01/2025-12-01/Main.hs
@@ -1,20 +1,38 @@
 module Main where
 
-data Rotation = L Int | R Int
+data Dir = L | R
+
+type Rotation = (Dir, Int)
 
 password :: [Rotation] -> Int
 password =
   let start = 50
       mod100 x = x `mod` 100
-      f pos rot = case rot of
-        L dist -> mod100 (pos - dist)
-        R dist -> mod100 (pos + dist)
-   in length . filter (== 0) . scanl f start
+      next pos (dir, dist) = case dir of
+        L -> mod100 (pos - dist)
+        R -> mod100 (pos + dist)
+      f (count, pos) rot = (count + zeros, nextPos)
+        where
+          zeros = count0s pos rot
+          nextPos = next pos rot
+   in fst . foldl' f (0, start)
+
+count0s :: Int -> Rotation -> Int
+count0s pos (dir, dist) =
+  let diff
+        | L <- dir = pos
+        | R <- dir = 100 - pos
+      fullRotations = (dist - diff) `div` 100
+      zeros
+        | diff == 0 = fullRotations
+        | dist >= diff = 1 + fullRotations
+        | otherwise = 0
+   in zeros
 
 parseContents :: String -> [Rotation]
 parseContents =
-  let f ('L' : xs) = L dist where dist = read xs :: Int
-      f ('R' : xs) = R dist where dist = read xs :: Int
+  let f ('L' : xs) = (L, dist) where dist = read xs :: Int
+      f ('R' : xs) = (R, dist) where dist = read xs :: Int
       f _ = error "Invalid rotation"
    in map f . lines
 
