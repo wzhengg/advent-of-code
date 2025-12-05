8177dd9e3fa76931dbbaeff5c54e6883d0f09c58
William Zheng <williamzheng2001@gmail.com>
2025-12-05 09:00:41 -0500

    2025 day 3 part 2

--- 2025/03/2025-12-05/Main.hs
+++ 2025/03/2025-12-05/Main.hs
@@ -1,21 +1,25 @@
-module Solution where
+module Main where
 
-totalJoltage :: [[Int]] -> Int
-totalJoltage = sum . map joltage
+totalJoltage :: [[Integer]] -> Integer
+totalJoltage =
+  let digits = 12
+   in sum . map (joltage digits)
 
-joltage :: [Int] -> Int
-joltage xs =
+joltage :: Int -> [Integer] -> Integer
+joltage 0 _ = 0
+joltage digits xs =
   let n = length xs
-      leftMax = maximum . take (n - 1) $ xs
-      right = drop 1 . dropWhile (/= leftMax) $ xs
-      rightMax = maximum right
-   in leftMax * 10 + rightMax
+      remaining = digits - 1
+      left = take (n - remaining) xs
+      max = maximum left
+      right = drop 1 . dropWhile (/= max) $ xs
+   in max * (10 ^ remaining) + joltage remaining right
 
-parseContents :: String -> [[Int]]
+parseContents :: String -> [[Integer]]
 parseContents =
   let parseLine l = case l of
         [] -> []
-        (x : xs) -> (read [x] :: Int) : parseLine xs
+        (x : xs) -> (read [x] :: Integer) : parseLine xs
    in map parseLine . lines
 
 main :: IO ()
