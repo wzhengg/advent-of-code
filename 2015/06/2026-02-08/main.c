7ef7aa485c52a06134ed73c36244b4615963230c
William Zheng <williamzheng2001@gmail.com>
2026-02-08 14:34:15 -0500

    2015 day 6 part 1

---
+++ 2015/06/2026-02-08/main.c
@@ -0,0 +1,96 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define N 1000
+#define LINES 300
+#define LINESIZE 32
+
+#define FALSE 0
+#define TRUE  1
+
+enum op { ON, OFF, TOGGLE };
+
+struct coordinate {
+	int r, c;
+};
+
+struct instruction {
+	enum op op;
+	struct coordinate topleft;
+	struct coordinate botright;
+};
+
+void do_instruction(char grid[][N], struct instruction instr) {
+	int left, right, top, bot, r, c;
+
+	left = instr.topleft.c;
+	right = instr.botright.c;
+	top = instr.topleft.r;
+	bot = instr.botright.r;
+
+	for (r = top; r <= bot; r++) {
+		for (c = left; c <= right; c++) {
+			switch (instr.op) {
+				case ON:
+					grid[r][c] = TRUE;
+					break;
+				case OFF:
+					grid[r][c] = FALSE;
+					break;
+				case TOGGLE:
+					grid[r][c] = !grid[r][c];
+					break;
+			}
+		}
+	}
+}
+long solve(char grid[][N], struct instruction *instrs) {
+	int i, r, c;
+	long res;
+
+	for (i = 0; i < LINES; i++)
+		do_instruction(grid, instrs[i]);
+
+	res = 0;
+	for (r = 0; r < N; r++)
+		for (c = 0; c < N; c++)
+			if (grid[r][c])
+				res++;
+
+	return res;
+}
+
+int main(void) {
+	FILE *fp;
+	char line[LINESIZE+2], grid[N][N];
+	struct instruction instrs[LINES];
+	int i;
+	long res;
+
+	fp = fopen("input.txt", "r");
+	if (!fp)
+		exit(1);
+
+
+	for (i = 0; i < LINES; i++) {
+		if (!fgets(line, LINESIZE+2, fp))
+			exit(1);
+
+		if (sscanf(line, "turn on %d,%d through %d,%d", &instrs[i].topleft.r, &instrs[i].topleft.c, &instrs[i].botright.r, &instrs[i].botright.c) == 4)
+			instrs[i].op = ON;
+		else if (sscanf(line, "turn off %d,%d through %d,%d\n", &instrs[i].topleft.r, &instrs[i].topleft.c, &instrs[i].botright.r, &instrs[i].botright.c) == 4)
+			instrs[i].op = OFF;
+		else if (sscanf(line, "toggle %d,%d through %d,%d\n", &instrs[i].topleft.r, &instrs[i].topleft.c, &instrs[i].botright.r, &instrs[i].botright.c) == 4)
+			instrs[i].op = TOGGLE;
+	}
+
+	if (fclose(fp))
+		exit(1);
+
+	memset(grid, 0, sizeof(grid));
+	res = solve(grid, instrs);
+	printf("%ld\n", res);
+
+	return 0;
+}
