49b6fd2961122a80b1a3987a706ce3bb4c38f544
William Zheng <williamzheng2001@gmail.com>
2026-04-30 11:24:22 -0400

    2016 day 3 part 2

--- 2016/03/2026-04-30/main.c
+++ 2016/03/2026-04-30/main.c
@@ -5,23 +5,35 @@
 
 int main(void) {
 	FILE *fp = fopen("input.txt", "r");
-	if (fp == NULL) {
+	if (fp == NULL)
 		exit(1);
-	}
 
-	char line[MAXLINE];
-	int a, b, c;
+	char line1[MAXLINE], line2[MAXLINE], line3[MAXLINE];
+	int a1, a2, a3;
+	int b1, b2, b3;
+	int c1, c2, c3;
 
 	int res = 0;
 
-	while (fgets(line, MAXLINE, fp) != NULL) {
-		if (sscanf(line, "%d %d %d", &a, &b, &c) != 3) {
+	while (fgets(line1, MAXLINE, fp) != NULL) {
+		if (fgets(line2, MAXLINE, fp) == NULL)
 			exit(1);
-		}
+		if (fgets(line3, MAXLINE, fp) == NULL)
+			exit(1);
 
-		if (a+b > c && b+c > a && a+c > b) {
+		if (sscanf(line1, "%d %d %d", &a1, &b1, &c1) != 3)
+			exit(1);
+		if (sscanf(line2, "%d %d %d", &a2, &b2, &c2) != 3)
+			exit(1);
+		if (sscanf(line3, "%d %d %d", &a3, &b3, &c3) != 3)
+			exit(1);
+
+		if (a1+a2 > a3 && a2+a3 > a1 && a1+a3 > a2)
 			++res;
-		}
+		if (b1+b2 > b3 && b2+b3 > b1 && b1+b3 > b2)
+			++res;
+		if (c1+c2 > c3 && c2+c3 > c1 && c1+c3 > c2)
+			++res;
 	}
 
 	printf("%d", res);
