51e760684a3ac454fe2e0b33c2de2e1cd07ae896
William Zheng <williamzheng2001@gmail.com>
2026-05-22 09:40:39 -0400

    2022 day 1 part 2

--- 2022/01/2026-05-22/main.c
+++ 2022/01/2026-05-22/main.c
@@ -1,6 +1,22 @@
+#include <assert.h>
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
+
+#define MAX_ELVES 300
+
+int cmp(const void *p1, const void *p2) {
+	long a = *(long *)p1;
+	long b = *(long *)p2;
+
+	if (a < b) {
+		return 1;
+	} else if (a > b) {
+		return -1;
+	} else {
+		return 0;
+	}
+}
 
 int main(void) {
 	FILE *fp = fopen("input.txt", "r");
@@ -8,24 +24,26 @@ int main(void) {
 		exit(1);
 	}
 
-	long max = 0;
+	long cals[MAX_ELVES] = {0};
+	int n = 0;
 	long cur = 0;
 
 	char buf[8];
 	while (fgets(buf, sizeof(buf), fp)) {
-		long cals = strtol(buf, NULL, 10);
-		if (cals) {
-			cur += cals;
-			continue;
+		long val = strtol(buf, NULL, 10);
+		if (val) {
+			cur += val;
+		} else {
+			assert(n < MAX_ELVES);
+			cals[n++] = cur;
+			cur = 0;
 		}
-
-		if (cur > max) {
-			max = cur;
-		}
-		cur = 0;
 	}
 
-	printf("%ld\n", max);
+	assert(n >= 3);
+	qsort(cals, n, sizeof(cals[0]), cmp);
+
+	printf("%ld\n", cals[0]+cals[1]+cals[2]);
 
 	return 0;
 }
