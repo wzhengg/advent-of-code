e47332217bf334ba35dcd18a0c90cda30f0ca01c
William Zheng <williamzheng2001@gmail.com>
2026-05-24 07:11:12 -0400

    2022 day 6 part 1

---
+++ 2022/06/2026-05-24/main.c
@@ -0,0 +1,30 @@
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAXLINE 4096
+
+int main(void) {
+	FILE *fp = fopen("input.txt", "r");
+	if (!fp) exit(1);
+
+	char buf[MAXLINE];
+	assert(fgets(buf, MAXLINE, fp));
+
+	long mask = 0;
+
+	int l, r;
+	for (l = r = 0; buf[r]; ++r) {
+		while (mask & (long)1<<(buf[r]-'a'))
+			mask &= ~((long)1 << (buf[l++]-'a'));
+
+		mask |= (long)1 << (buf[r]-'a');
+
+		if (r-l+1 == 4)
+			break;
+	}
+
+	printf("%d\n", r+1);
+
+	return 0;
+}
